b21bbe83275c25a396d7cf54463ff77e4d659b74
VladislavGl <glukhov.vl04@mail.ru>
2026-05-12 00:37:56 +0500

    structure update

--- frontend/android/app/src/main/kotlin/com/example/maincrm/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.maincrm
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- frontend/lib/features/documents/domain/repositories/task_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/task.dart';
-
-abstract class TaskRepository {
-  Future<List<Task>> getTasks();
-  Future<void> addTask(Task task);
-  Future<void> updateTask(Task task);
-  Future<void> deleteTask(String id);
-}
